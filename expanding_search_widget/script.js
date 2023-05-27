e62c209ca9a07fc8eacd00f203eb709ee226f4be
Vudutha Venkata Surya kiran <114286141+venkat-devish@users.noreply.github.com>
2023-05-27 10:22:16 +0530

    fix: focus on click

--- expanding_search_widget/script.js
+++ expanding_search_widget/script.js
@@ -1,6 +1,16 @@
-const btn = document.querySelector('.btn');
-const search = document.querySelector('.search');
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('input')
 
 btn.addEventListener('click', () => {
     search.classList.toggle('active')
+    input.focus();
+})
+
+input.addEventListener('blur', (e) => {
+    let val = e.target.value;
+    if (val === "") {
+        search.classList.remove('active');
+        val = "";
+    }
 })
